f6c9a2da17b64cc45668b44c65324e3740aee7aa
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-07 18:09:02 +0530

    updateNotes working fine📝

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const btn = document.getElementById("btn");
 const noteContainer = document.querySelector(".notes-container");
 
+getNotes().forEach((note) => {
+  const noteEl = createNoteEl(note.id, note.content);
+  noteContainer.insertBefore(noteEl, btn);
+});
+
 function createNoteEl(id, content) {
   const textArea = document.createElement("textarea");
   textArea.classList.add("note-card");
-  textArea.placeholder = "Empty note";
+  textArea.placeholder = "Empty Note";
   textArea.value = content;
 
   const deleteNote = () => {};
-  const updateNote = () => {};
-  textArea.addEventListener("dblclick", (e) => {
+  const updateNote = (id, content) => {
+    const notes = getNotes();
+    const targetedNote = notes.filter((note) => note.id == id)[0];
+    targetedNote.content = content;
+    saveNotes(notes);
+  };
+
+  textArea.addEventListener("dblclick", () => {
     const warning = confirm("Do you really want to delete this Note?");
     if (warning) {
       deleteNote(id, textArea);
@@ -22,12 +33,21 @@ function createNoteEl(id, content) {
 }
 
 function addNote() {
+  const notes = getNotes();
   const noteObject = {
-    id: Math.floor(Math.random() * 1000),
+    id: Math.floor(Math.random() * 100000),
     content: "",
   };
   const noteEl = createNoteEl(noteObject.id, noteObject.content);
   noteContainer.insertBefore(noteEl, btn);
+  notes.push(noteObject);
+  saveNotes(notes);
+}
+function saveNotes(notes) {
+  localStorage.setItem("note-dashboard", JSON.stringify(notes));
+}
+function getNotes() {
+  return JSON.parse(localStorage.getItem("note-dashboard") || "[]");
 }
 
 btn.addEventListener("click", addNote);
